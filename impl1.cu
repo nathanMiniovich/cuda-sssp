b9c84a417156a6cb2ac2c3232ab16d667788b2f6
nathanMiniovich <nathan.miniovich@rutgers.edu>
2017-03-24 15:45:33 -0400

    better skeleton

--- impl1.cu
+++ impl1.cu
@@ -8,7 +8,7 @@
 
 using namespace std
 
-__global__ void pulling_kernel(vector<initial_vertex> * peeps, int offset, int * anyChange){
+__global__ void edge_process(vector<initial_vertex> * neighbours, vector<int>* distance_prev, vector<int>* distance_cur, int offset, int * anyChange){
 
 	//update me based on my neighbors. Toggle anyChange as needed.
 	//offset will tell you who I am.
@@ -18,18 +18,18 @@ void swap(vector<int>* distance_cur, vector<int>* distance_prev){
 	
 }
 
-void puller(vector<initial_vertex> * peeps, int blockSize, int blockNum){
+void puller(vector<initial_vertex> * neighbours, int blockSize, int blockNum){
 	setTime();
 
 	//housekeeping goes here
 	//populate distance_prev & distance_cur
 	
 	for(int i=0; i < peeps->size()-1; i++){
-		pulling_kernel<<<blockNum,blockSize>>>(peeps,/*distance_prev*/,/*distance_cur*/);
-		if(/*no node is changed*/){
+		pulling_kernel<<<blockNum,blockSize>>>(neighbours, distance_prev, distance_cur, 0, anyChange);
+		if(anyChange[0]){
 			break;
 		} else {
-			swap(/*distance_cur*/,/*distance_prev*/)
+			swap(distance_cur,distance_prev)
 		}
 	}
 
