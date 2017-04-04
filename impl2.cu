40bb98adcd27e9c3d7884168237732f7d87da866
Augustus Chang <augustus.chang@rutgers.edu>
2017-04-04 18:17:35 -0400

    add populate kernel

--- impl2.cu
+++ impl2.cu
@@ -13,6 +13,23 @@ __global__ void neighborHandling_kernel(std::vector<initial_vertex> * peeps, int
     //Offset will tell you who I am.
 }
 
+        
+__global__ void populate(const edge_node *L, const unsigned int edge_num, unsigned int *distance_prev, unsigned int *distance_cur){
+    
+    int thread_id = blockDim.x * blockIdx.x + threadIdx.x;
+    int thread_num = blockDim.x * gridDim.x;
+
+    int warp_id = thread_id/32;
+    int warp_num = thread_num/32 + 1;
+    int lane_id = thread_id % 32;
+
+    int load = (edge_num % warp_num == 0) ? edge_num/warp_num : edge_num/warp_num+1;
+    int beg = load * warp_id;
+    int end = min(edge_num, beg+ load);
+    beg += lane_id;
+
+}
+
 void neighborHandler(std::vector<initial_vertex> * peeps, int blockSize, int blockNum){
     setTime();
 
