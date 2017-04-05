b56ccc713b72dd1b63527afa2baa4f4a6913d402
Augustus Chang <augustus.chang@rutgers.edu>
2017-04-04 20:31:40 -0400

    counted to-process edges

--- impl2.cu
+++ impl2.cu
@@ -14,19 +14,36 @@ __global__ void neighborHandling_kernel(std::vector<initial_vertex> * peeps, int
 }
 
         
-__global__ void populate(const edge_node *L, const unsigned int edge_num, unsigned int *distance_prev, unsigned int *distance_cur){
+// step 1
+__global__ void getX(const edge_node *L, const unsigned int edge_num, unsigned int *pred, unsigned int *X){
     
     int thread_id = blockDim.x * blockIdx.x + threadIdx.x;
     int thread_num = blockDim.x * gridDim.x;
 
     int warp_id = thread_id/32;
-    int warp_num = thread_num/32 + 1;
+    int warp_num = (thread_num % 32 == 0) ? thread_num/32 : edge_num/32 + 1;
     int lane_id = thread_id % 32;
 
+    // how many edges each warp takes
     int load = (edge_num % warp_num == 0) ? edge_num/warp_num : edge_num/warp_num+1;
     int beg = load * warp_id;
     int end = min(edge_num, beg+ load);
     beg += lane_id;
+
+    unsigned int num = 0;
+    for(int i = beg; i < end; i+=32){
+	int mask = __ballot(pred[L[i].srcIndex]);
+	if(lane_id == 0){
+	    num += (unsigned int) __popc(mask);
+	}
+    }
+
+    if(lane_id == 0){
+	X[warp_id] = num;
+    }
+}
+
+__global__ void getY(unsigned int *X, unsigned int *Y){
 
 }
 
@@ -35,7 +52,10 @@ void neighborHandler(std::vector<initial_vertex> * peeps, int blockSize, int blo
 
     /*
      * Do all the things here!
-     **/
+     *
+     */
+
+
 
     std::cout << "Took " << getTime() << "ms.\n";
 }
